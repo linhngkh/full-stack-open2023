34d7e348d6320c2f7c67556bd3fe92a7202b4b19
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-24 22:10:08 +0300

    Step1

---
+++ part-2/part2.6-2.10/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <div>debug: {newName}</div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
+
+export default App;
