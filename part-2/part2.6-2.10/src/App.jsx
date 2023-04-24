31e9a435760a164a2a495c45bf0eaee351207bcb
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-24 22:41:28 +0300

    render person comp

--- part-2/part2.6-2.10/src/App.jsx
+++ part-2/part2.6-2.10/src/App.jsx
@@ -1,28 +1,68 @@
 import { useState } from "react";
+import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
 
-  const handleChange = (event) => {
-    event.preventDefault();
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleChangeName = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const nameExist = persons.find((person) => person.name === newName);
+    if (nameExist) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      const nameObject = {
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1,
+      };
+      setPersons(persons.concat(nameObject));
+      setNewName("");
+      setNewNumber("");
+    }
   };
 
   return (
     <div>
-      <div>debug: {newName}</div>
       <h2>Phonebook</h2>
-      <form>
+
+      <div>filter shown with</div>
+      <h2>add a new</h2>
+      <form onSubmit={addName}>
         <div>
-          name: <input value={newName} onChange={handleChange} />
+          name: <input value={newName} onChange={handleChangeName} />
         </div>
+        <br />
+        <div>
+          number: <input value={newNumber} onChange={handleChangeNumber} />
+        </div>
+        <br />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <Persons person={person} key={person.id} />
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ part-2/part2.6-2.10/src/components/Persons.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Persons = ({ person }) => {
+  return (
+    <div>
+      <h4>{person.name}</h4>
+    </div>
+  );
+};
+
+export default Persons;
