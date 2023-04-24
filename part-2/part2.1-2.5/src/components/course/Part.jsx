53d00547541d7a6d69c8c210af7f06afa0b8f1e1
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-24 21:14:41 +0300

    use reduce method

--- part-2/part2.1-2.5/src/components/course/Part.jsx
+++ part-2/part2.1-2.5/src/components/course/Part.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-const Part = ({ course: { parts } }) => {
-  const total = parts.reduce((acc, curr) => {
-    return acc + curr.exercises;
+const Part = ({ courses }) => {
+  const total1 = courses[0].parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+  const total2 = courses[1].parts.reduce(
+    (sum, part) => sum + part.exercises,
+    0
+  );
+  const total = courses.reduce((sum, course) => {
+    return (
+      sum +
+      course.parts.reduce((acc, part) => {
+        return acc + part.exercises;
+      }, 0)
+    );
   }, 0);
   return (
     <div>
-      <h5>
-        {parts[0].name} {parts[0].exercises}
+      {/* <h5>
+        {courses.parts[0].name} {courses.parts[0].exercises}
       </h5>
       <h5>
         {parts[1].name} {parts[1].exercises}
@@ -17,7 +30,9 @@ const Part = ({ course: { parts } }) => {
       </h5>
       <h5>
         {parts[3].name} {parts[3].exercises}
-      </h5>
+      </h5> */}
+      <p>total of {total1}</p>
+      <p>total of {total2}</p>
       <strong>total of {total} exercises</strong>
     </div>
   );
