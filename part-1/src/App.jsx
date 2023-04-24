857fadd6dc10d64526c3bf7b1326d4bb6d8a5625
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-24 14:29:25 +0300

    1.12*: anecdotes step1

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import FirstPart from "./components/parts1.1-1.5/FirstPart";
+import FirstPart from "./components/parts1.1-1.11/FirstPart";
 import SecondPart from "./components/part1.6-1.14/SecondPart";
+import Anecdotes from "./components/anecdotes/Anecdotes";
 const App = () => {
   return (
     <div>
       <div>
-        <h1>Part 1.1 -1.5</h1> <FirstPart />
+        <h1>Part 1.1 -1.5 </h1> <FirstPart />
       </div>
       <div>
-        <h1>Part1.6-1.14</h1>
+        <h1>Part1.6-1.11 Unicafe</h1>
         <SecondPart />
+      </div>
+      <div>
+        <h1>Part 1.12 - 1.14</h1>
+        <Anecdotes />
       </div>
     </div>
   );
