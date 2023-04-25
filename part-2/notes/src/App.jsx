463829e76121194995aa87d00baf8697176be8ed
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-25 11:40:14 +0300

    rename part

---
+++ part-2/notes/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import Note from "./Note";
+
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const noteToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNewNote(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {noteToShow.map((note) => (
+          <Note note={note} key={note.id} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
