4e9efd5e86a4b3330b9994a5728bd3a1be1cae6a
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-25 18:52:42 +0300

    done

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -6,13 +6,7 @@ import phoneBookService from "./services/server";
 import Notification from "./components/Notification";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
@@ -69,13 +63,14 @@ const App = () => {
           }, 3000);
         })
         .catch((error) => {
-          console.log(error);
+          console.log("fail to create new", error);
           setErrorMessage(error.response.data.error);
           setTimeout(() => {
             setErrorMessage(null);
           }, 3000);
         });
     } else {
+      // update the exist one
       if (window.confirm(`Update ${nameExist[0].name}?`)) {
         const personFound = persons.find(
           (person) => person.id === nameExist[0].id
@@ -83,16 +78,14 @@ const App = () => {
         const changePerson = { ...personFound, number: newNumber };
         phoneBookService
           .update(changePerson.id, changePerson)
-          .then(
+          .then(() => {
             setPersons(
               persons.map((p) => (p.id !== changePerson.id ? p : changePerson))
-            )
-          )
-          .catch((error) => {
-            console.log(error);
-            setErrorMessage(
-              `${personFound.name} was already deleted from server`
             );
+            setSuccessAdded(`Update ${changePerson.name} successfully`);
+          })
+          .catch((error) => {
+            console.log("fail to update", error);
           });
         setTimeout(() => {
           setErrorMessage(null);
@@ -113,30 +106,12 @@ const App = () => {
           setSuccessAdded(`Deleted ${personToDelete.name}`);
         })
         .catch((error) => {
-          console.log(error);
-          setErrorMessage(error.message);
+          console.log("fail to delete ", error);
+          setErrorMessage(
+            `Information of ${personToDelete.name} has already been removed from server`
+          );
         });
     }
-  };
-  // step10
-  const updatePerson = (personToUpdate) => {
-    const updatedPerson = { ...personToUpdate, number: newNumber };
-    phoneBookService
-      .update()
-      .then((res) => {
-        setPersons(
-          persons.map((p) => (p.id === personToUpdate.id ? res.data : p))
-        );
-        setNewName("");
-        setNewNumber("");
-        setSuccessAdded(
-          `${personToUpdate.name} is already added to phonebook, replace the old number with a new one?`
-        );
-      })
-      .catch((error) => {
-        console.log(error);
-        setErrorMessage(error.message);
-      });
   };
 
   return (
@@ -149,7 +124,7 @@ const App = () => {
       ) : null}
 
       <Filter filterHandle={filterHandle} filterByName={filterByName} />
-      <h2>Add a new</h2>
+      <h2>Add a new person</h2>
       <PersonForm
         addName={addName}
         newName={newName}
