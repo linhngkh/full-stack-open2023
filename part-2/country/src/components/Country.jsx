aad2ce64d47a13740d4523b6f54d3dd0f0bac140
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-26 17:23:44 +0300

    2.20*: Data for countries, step3

--- part-2/country/src/components/Country.jsx
+++ part-2/country/src/components/Country.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+
+  const URL = "https://api.openweathermap.org/data/2.5/weather?";
+  const API_KEY = "c2ccd68ef98004ae2058dcec9fe13a58";
+
+  useEffect(() => {
+    axios.get(URL + `q=${country[0].capital}&appid=${API_KEY}`).then((res) => {
+      setWeather(res.data);
+    });
+  }, [country]);
+
   return (
     <div>
       <h1>{country[0].name.common}</h1>
@@ -14,6 +26,24 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img src={country[0].flags.png} alt="nationalFlag" />
+
+      {/* weather */}
+      <h3>Weather in {country[0].capital}</h3>
+
+      {weather.main && (
+        <>
+          {" "}
+          <p>
+            Temperature {(weather.main.temp - 273.15).toFixed(2)}
+            <sup>&deg;C</sup>
+          </p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>Wind {weather.wind.speed} m/s</p>
+        </>
+      )}
     </div>
   );
 };
