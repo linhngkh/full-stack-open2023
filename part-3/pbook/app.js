c2262ad3d5c213c253efed843b5a089d35d76852
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-26 21:04:35 +0300

    Phonebook backend step6

--- part-3/pbook/app.js
+++ part-3/pbook/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const PORT = 3002;
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -24,8 +26,6 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
-
-app.use(express.json());
 
 app.get("/info", (req, res) => {
   let d = Date(Date.now());
@@ -50,34 +50,35 @@ app.get("/api/persons/:id", (req, res) => {
 });
 
 const generatedId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
-  return maxId + 1;
+  const uniqueId = Math.floor(Math.random() * 100);
+  return uniqueId + 1;
 };
 
 app.post("/api/persons", (req, res) => {
   const body = req.body;
-  if (!body.content) {
+  if (!body.name || !body.number) {
     //   400 Bad Request
     return res.status(400).json({
       error: "The name or number is missing.",
     });
   }
-  // 409 Conflict
-  else if (body.content) {
-    return res.status(409).json({
-      error: "Name must be unique",
-    });
-  }
 
   const person = {
-    content: body.content,
-    important: body.important || false,
     id: generatedId(),
+    name: body.name,
+    number: body.number,
   };
 
   persons = persons.concat(person);
   // 201 Created
-  res.status(201).json({ message: "New entry created successfully" });
+  res.json(person);
+
+  // 409 Conflict
+  if (body.name) {
+    return res.status(409).json({
+      error: "Name must be unique",
+    });
+  }
 });
 
 app.delete("/api/persons/:id", (req, res) => {
