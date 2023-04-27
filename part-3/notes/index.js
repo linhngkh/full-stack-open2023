124b1749dadb77fc58298ff184303c8fd3efd02b
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-27 21:43:12 +0300

    deploy front end

--- part-3/notes/index.js
+++ part-3/notes/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
