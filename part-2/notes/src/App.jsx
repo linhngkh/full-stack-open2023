d9cc1081d10242feb6eba94dc527ddf43aa9c721
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-27 21:30:56 +0300

    https://sparkling-pine-4853.fly.dev/api/notes

--- part-2/notes/src/App.jsx
+++ part-2/notes/src/App.jsx
@@ -36,7 +36,6 @@ const App = () => {
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
-
     noteService
       .update(id, changedNote)
       .then((returnedNote) => {
