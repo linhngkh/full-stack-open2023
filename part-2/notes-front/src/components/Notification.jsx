f55322757eaf9afca5e950a684e783c4a6faa94f
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-29 16:10:11 +0300

    config proxy in vite

--- part-2/notes-front/src/components/Notification.jsx
+++ part-2/notes-front/src/components/Notification.jsx
@@ -4,7 +4,7 @@ const Notification = ({ message }) => {
   if (message === null) {
     return null;
   }
-  return <div className="success">{message}</div>;
+  return <div className="error">{message}</div>;
 };
 
 export default Notification;
