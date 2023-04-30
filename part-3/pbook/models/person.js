07626689938953fab2c5776c6cae434539f775cb
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-04-30 14:13:42 +0300

    update phonebook backend

--- part-3/pbook/models/person.js
+++ part-3/pbook/models/person.js
@@ -19,20 +19,12 @@ const phoneSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", phoneSchema);
 
-if (process.argv[3]) {
-  const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4],
-  });
-  person.save().then((result) => {
-    console.log(`added ${person.name} ${person.number} to phonebook`);
-  });
-} else {
-  console.log("phonebook:");
-  Person.find({}).then((result) => {
-    result.forEach((person) => {
-      console.log(`${person.name} ${person.number}`);
-    });
-    mongoose.connection.close();
-  });
-}
+phoneSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", phoneSchema);
