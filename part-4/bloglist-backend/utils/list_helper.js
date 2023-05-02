485da41d1848d1020a4aa35be12a68cfc9165c60
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-02 22:22:16 +0300

    4.4: helper functions and unit tests, step2

--- part-4/bloglist-backend/utils/list_helper.js
+++ part-4/bloglist-backend/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
   return (blogs = 1);
 };
 
+const totalLikes = (posts) => {
+  return posts.reduce((total, post) => total + post.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
