11ee7bdf78c72206fc8cc3b92485e9ec0649a558
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-03 08:34:32 +0300

    testing backend part

--- part-4/bloglist-backend/utils/list_helper.js
+++ part-4/bloglist-backend/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (posts) => {
   return posts.reduce((total, post) => total + post.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const reduceBlog = blogs.reduce((max, blog) =>
+    max.likes > blog.likes ? max : blog
+  );
+  const { title, author, likes } = reduceBlog;
+  return { title, author, likes };
+};
+
+// const favoriteBlog = (blogs) => {
+//   const sortBlog = blogs.sort((a, b) => b.likes - a.likes);
+//   return sortBlog[0];
+// };
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
