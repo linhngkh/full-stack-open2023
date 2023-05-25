9aa644fd65c0e157e9e6a6f5805ec3c67f5af387
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-05-25 16:40:17 +0300

    5.17: bloglist end to end testing, step1

--- part-5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ part-5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -1,6 +1,13 @@
 describe("Blog app", function () {
   beforeEach(function () {
-    cy.request("POST", "http://localhost:3000");
+    cy.request("POST", "http://localhost:3003/api/testing/reset");
+    // create here a user to backend
+    const user = {
+      name: "root",
+      username: "Superuser",
+      password: "sekret",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
