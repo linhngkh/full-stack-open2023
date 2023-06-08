ea4fa83f1b5525b7eb5669e32f83657ea013ed3e
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-08 17:56:20 +0300

    6.14 Anecdotes and the backend, step1

--- part-6/toolkit-anecdotes/src/App.js
+++ part-6/toolkit-anecdotes/src/App.js
@@ -2,8 +2,16 @@ import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { useEffect } from "react";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initializeAnecdotes());
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part-6/toolkit-anecdotes/src/components/AnecdoteForm.js
+++ part-6/toolkit-anecdotes/src/components/AnecdoteForm.js
@@ -1,13 +1,16 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { handleNotification } from "../reducers/notifyReducer";
+import anecdoteService from "../services/anecdoteService";
 
-const AnecdoteForm = () => {
+const AnecdoteForm = (props) => {
   const dispatch = useDispatch();
+
   const addAnecdote = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.anecdote.value = "";
+
     dispatch(createAnecdote(content));
     dispatch(handleNotification(`You created "${content}"`, 5));
   };
