ae1babbb5443d5c1f9162712258526cc2e56c35f
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-16 16:39:30 +0300

    7.7: country hook

---
+++ part-7/country-hook/src/hooks./index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+export const useField = (type) => {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return {
+    type,
+    value,
+    onChange,
+  };
+};
+
+export const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    if (name) {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${name}`)
+        .then((response) => setCountry(response.data[0]))
+        .catch((error) => setCountry(null));
+    }
+  }, [name]);
+
+  return country;
+};
