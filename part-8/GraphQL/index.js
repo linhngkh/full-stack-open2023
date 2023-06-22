cd56aa41d7fbfaaea58e912abe1353c40e297838
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-06-22 22:00:45 +0300

    object within an object

---
+++ part-8/GraphQL/index.js
@@ -0,0 +1,79 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    phone: "040-123543",
+    street: "Tapiolankatu 5 A",
+    city: "Espoo",
+    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Matti Luukkainen",
+    phone: "040-432342",
+    street: "Malminkaari 10 A",
+    city: "Helsinki",
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
+  },
+  {
+    name: "Venla Ruuska",
+    street: "Nallemäentie 22 C",
+    city: "Helsinki",
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
+  },
+];
+
+//graphQL schema
+const typeDefs = `
+type Address {
+  street: String!
+  city: String! 
+}
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    city: String! 
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`;
+// populating the data for a single field in schema
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
+    name: (root) => root.name,
+    phone: (root) => root.phone,
+    street: (root) => root.street,
+    city: (root) => root.city,
+    id: (root) => root.id,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
