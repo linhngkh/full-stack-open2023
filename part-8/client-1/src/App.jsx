fb3806c1404921a864c1758a2558b2dff1f22fee
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-31 21:45:04 +0300

    added notify to form

--- part-8/client-1/src/App.jsx
+++ part-8/client-1/src/App.jsx
@@ -2,18 +2,30 @@ import { useQuery } from "@apollo/client";
 import Persons from "./components/Persons";
 import { ALL_PERSONS } from "./queries";
 import PersonForm from "./components/PersonForm";
+import { useState } from "react";
+import Notify from "./components/Notify";
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000,
   });
   if (result.loading) {
     return <div>Loading ...</div>;
   }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 10000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 }
