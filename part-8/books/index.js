585b3be6e701f0bd613a07a045766ca04dd1345e
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-07-31 19:01:38 +0300

    delete comments

--- part-8/books/index.js
+++ part-8/books/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { gql } = require("apollo-server");
 
 let authors = [
   {
@@ -26,18 +27,6 @@ let authors = [
     id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
   },
 ];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- 
- 
- */
 
 let books = [
   {
@@ -91,39 +80,33 @@ let books = [
   },
 ];
 
-/*
-  you can remove the placeholder query once your first own has been implemented 
-*/
-
-const typeDefs = `
-type Authors {
-    name: [String!]!
+const typeDefs = gql`
+  type Author {
+    name: String!
+    born: Int
     bookCount: Int!
-}
+    id: ID!
+  }
 
-type Books {
+  type Books {
     title: String!
     author: String!
     published: Int!
     genres: [String!]!
-}
-
-  type Query {
-    bookCount: Int!
-    authorCount: Int!
-    allBooks: [Books!]!
   }
 
-
-
+  type Query {
+    authorCount: Int!
+    allBooks: [Books!]!
+    allAuthors: [Author!]!
+  }
 `;
 
 const resolvers = {
   Query: {
-    bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
-    // allAuthors: () => authors.name,
+    allAuthors: () => authors.find({}),
   },
 };
 
